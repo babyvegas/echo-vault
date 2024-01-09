7112abd1e32ad3008aefdd386e9cb8e21707eb13
Donovan Barrientos <donovan_ba@outlook.com>
2024-01-09 00:14:53 -0600

    terminado template principal

--- echo-vault/src/app/app.module.ts
+++ echo-vault/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
